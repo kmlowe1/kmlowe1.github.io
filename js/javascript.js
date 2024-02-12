f4fba268547d617a4728e13de84b9b2d5411255c
Kevin Lowe <kmlowe1@gmail.com>
2024-02-12 17:13:15 +0000

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -7,17 +7,8 @@ function increaseFontSize(id, increaseFactor){
      txt.style.fontSize = (currentSize + changeFactor) + 'px';
 }
 function decreaseFontSize(id, decreaseFactor){
-	txt = document.getElementById(id);
-	style = windo.getComputedStyle(txt, null).getPropertyValue('font-size');
-	currentSize = parseFloat(style);
-	txt.style.fontSize = (currentSize - changeFactor) + 'px';
+	 txt = document.getElementById(id);
+	 style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+	 currentSize = parseFloat(style);
+	 txt.style.fontSize = (currentSize - changeFactor) + 'px';
 }
-
-
-/**
-But, if we want a "Decrease Font Size 1px", what we can do? We call the function with -1 rather than with 1.
-
-We solve the Decrease Font Size problem as well. 
-However, I would change the function name to a more generic one and call it in both two 
-functions that I would create: increaseFontSize(id, increaseFactor) and 
-decreaseFontSize(id, decreaseFactor).**/
