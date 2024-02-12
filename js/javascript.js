6958590d1ebefa050419a7576cd8a6cffd753fcd
Kevin Lowe <kmlowe1@gmail.com>
2024-02-12 15:25:52 +0000

    adding javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,20 @@
+var changeFactor = 1;
+
 function increaseFontSize(id, increaseFactor){
      txt = document.getElementById(id);
      style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
      currentSize = parseFloat(style);
-     txt.style.fontSize = (currentSize + increaseFactor) + 'px';
+     txt.style.fontSize = (currentSize + changeFactor) + 'px';
+}
+function decreaseFontSize(id, decreaseFactor){
+	txt = document.getElementById(id);
+	style = windo.getComputedStyle(txt, null).getPropertyValue('font-size');
+	currentSize = parseFloat(style);
+	txt.style.fontSize = (currentSize - changeFactor) + 'px';
 }
 
+
 /**
-<input type="button" value="Increase Font Size 1px" onclick="increaseFontSize("b", 1)">
-<p id="b">Font Size by 1 Pixel</p>
-
-<input type="button" value="Decrease Font Size 1px" onclick="increaseFontSize("b", -1)">
-<p id="b">Font Size by -1 Pixel</p>
-
 But, if we want a "Decrease Font Size 1px", what we can do? We call the function with -1 rather than with 1.
 
 We solve the Decrease Font Size problem as well. 
