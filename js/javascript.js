276ee46338e686e456cced1b42c4e794e0976cee
Kevin Lowe <kmlowe1@gmail.com>
2024-02-12 18:08:13 +0000

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,29 @@
-var changeFactor = 5;
+var changeFactor = 5;  /**Constant for increase/decrease font size factor**/
 
-function increaseFontSize(id, increaseFactor){
-     txt = document.getElementById(id);
-     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
-     currentSize = parseFloat(style);
-     txt.style.fontSize = (currentSize + changeFactor) + 'px';
+function getCurrentFontSize(){
+	 txt = document.getElementById(id);
+	 style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+	 return currentSize = parseFloat(style);
 }
+
+/** This function increases the font size of the text in the HTML Element **/
+function increaseFontSize(id, increaseFactor){
+/**     txt = document.getElementById(id);
+     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+     currentSize = parseFloat(style);**/
+     txt.style.fontSize = (getCurrentFontSize() + changeFactor) + 'px';
+}
+/** This function decreases the font size of the text in the HTML Element **/
 function decreaseFontSize(id, decreaseFactor){
 	 txt = document.getElementById(id);
 	 style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
 	 currentSize = parseFloat(style);
 	 txt.style.fontSize = (currentSize - changeFactor) + 'px';
 }
+
+function pageUp(){
+}
+
+function pageDown(){
+}
+
