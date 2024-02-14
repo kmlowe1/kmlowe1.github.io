164d0ea2da3a97e92829a372c049a3b276014828
Kevin Lowe <kmlowe1@gmail.com>
2024-02-14 15:25:21 +0000

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ var changeFactor = 5;  /**Constant for increase/decrease font size factor**/
 
 /** This function increases the font size of the text in the HTML Element **/
 function increaseFontSize(id, increaseFactor){
-     txt = document.getElementById(id);
+     txt = parent.document.getElementById(id);
      style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
      currentSize = parseFloat(style);
      txt.style.fontSize = (currentSize + changeFactor) + 'px';
