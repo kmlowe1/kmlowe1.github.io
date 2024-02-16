b8c60ab59895aad56c3918c832c7f803635c306e
Kevin Lowe <kmlowe1@gmail.com>
2024-02-16 02:14:48 +0000

    Redesign of layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,20 @@
-var changeFactor = 5;  /**Constant for increase/decrease font size factor**/
+var changeFactor = 1;  /**Constant for increase/decrease font size factor**/
 
 /** This function increases the font size of the text in the HTML Element **/
-function increaseFontSize(id, increaseFactor){
-     txt = parent.document.getElementById(id);
+function increaseFontSize(id) {
+     txt = document.getElementById(id);
      style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
      currentSize = parseFloat(style);
-     txt.style.fontSize = (currentSize + changeFactor) + 'px';
+     txt.style.fontSize = (currentSize + changeFactor) + 'em';
 }
 /** This function decreases the font size of the text in the HTML Element **/
-function decreaseFontSize(id, decreaseFactor){
-	 txt = parent.document.getElementById(id);
-	 style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
-	 currentSize = parseFloat(style);
-	 txt.style.fontSize = (currentSize - changeFactor) + 'px';
-}
-
-function pageUp(){
-}
-
-function pageDown(){
+function decreaseFontSize(id) {
+     txt = document.getElementById(id);
+     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+     currentSize = parseFloat(style);
+     if(currentSize <= 1){
+          return;
+     }
+     txt.style.fontSize = (currentSize - changeFactor) + 'em';
 }
 
